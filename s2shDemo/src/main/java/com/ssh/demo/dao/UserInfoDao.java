cd3079cc9de85262b4a9fdf267724d58e87d601c
xiaokun4 <xiaokun4@lenovo.com>
2016-08-16 17:36:31 +0800

    ssh

---
+++ s2shDemo/src/main/java/com/ssh/demo/dao/UserInfoDao.java
@@ -0,0 +1,14 @@
+package com.ssh.demo.dao;
+
+import com.ssh.demo.entity.UserInfo;
+
+/**  
+ * 创建时间：2015-2-13 下午12:49:55  
+ * @author andy  
+ * @version 2.2  
+ * 描述： userDao 
+ */
+
+public interface UserInfoDao extends GenericDao<UserInfo, Integer> {
+
+}
---
+++ s2shDemo/src/test/java/com/ssh/demo/test/TestUserService.java
@@ -0,0 +1,41 @@
+package com.ssh.demo.test;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.alibaba.fastjson.JSON;
+import com.ssh.demo.entity.UserInfo;
+import com.ssh.demo.service.UserInfoService;
+
+/**
+ * 创建时间：2015-2-13 下午3:31:07
+ * 
+ * @author andy
+ * @version 2.2
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:spring.xml",
+		"classpath:spring-hibernate.xml" })
+public class TestUserService {
+
+	private static final Logger LOGGER = Logger
+			.getLogger(TestUserService.class);
+
+	@Autowired
+	private UserInfoService userInfoService;
+
+	@Test
+	public void save() {
+		UserInfo userInfo = new UserInfo();
+		userInfo.setName("zty");
+		userInfo.setAge(23);
+		userInfo.setTelephone("13212221333");
+		Integer id = userInfoService.save(userInfo);
+		JSON.toJSONString(id);
+	}
+
+}
